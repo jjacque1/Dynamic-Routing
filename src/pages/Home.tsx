ca7256132ee9adee61b4072fe48a0d2a806e0eb6
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-28 18:02:38 -0500

    added some styling and a fommating

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     return (
         <div>
             <h2>Home Page</h2>
-            
+            <h2>Click "Blogs" or "Log In" to get started</h2>
         </div>
     )
 }
